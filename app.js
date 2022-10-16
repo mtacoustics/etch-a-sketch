41a201f0d48b0e2016af4c6f56be8b1250b01f5e
Michael Hughes <Michaelhughes5310@gmail.com>
2022-10-16 16:32:09 -0400

    Defined create rows function. For loop to create the rows.

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const container = document.getElementById("gridContainer")
-const button = document.getElementById("changeGrid")
+const gridContainer = document.getElementById("gridContainer")
+const rows = document.getElementById("gridRow")
+const columns = document.getElementById("gridColumns")
+const gridButton = document.getElementById("changeGrid")
+
+// create 16 x 16 grid
+
+function defaultgrid() {
+    createRows(16);
+    createColumns(16);
+}
+
+function createRows(rows) {
+    for (let i = 0; i < rows; i++ ) {
+        let row = document.createElement("div")
+        gridContainer.appendChild(row).className("gridRow")
+    }
+}
+
+function createColumns(columns) {
+    
+}
