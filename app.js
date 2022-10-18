59c8958590227863d6f71d8ef0b8be59e35446a1
Michael Hughes <Michaelhughes5310@gmail.com>
2022-10-18 09:04:32 -0400

    Finally worked out the functions to create the grid. also included a default function that gets called on load. just a 16 x 16 grid.

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-const gridContainer = document.getElementById("gridContainer")
-const rows = document.getElementById("gridRow")
-const columns = document.getElementById("gridColumns")
+const gridContainer = document.querySelector("#gridContainer")
 const gridButton = document.getElementById("changeGrid")
+const cells = document.getElementsByClassName('cells')
+const row = document.getElementsByClassName('rows')
 
-// create 16 x 16 grid
+document.onload = createDefaultGrid();
 
-function defaultgrid() {
-    createRows(16);
-    createColumns(16);
+function createDefaultGrid() {
+    createRow(16);
+    fillRows(16);
 }
 
-function createRows(rows) {
-    for (let i = 0; i < rows; i++ ) {
-        let row = document.createElement("div")
-        gridContainer.appendChild(row).className("gridRow")
+function createRow (row) {
+    for (let i = 0; i < row; i++) {
+        const rows = document.createElement("div");
+        gridContainer.appendChild(rows);
+        rows.classList.add('rows');
     }
+    const rows = document.getElementsByClassName('rows')
 }
 
-function createColumns(columns) {
-    
+function fillRows (columns) {
+    for (let i = 0; i < row.length; i++) {
+        for (n = 0; n < columns; n++) {
+            let cells = document.createElement('div')
+            row[n].appendChild(cells)
+            cells.classList.add('cells')
+        }
+    }
 }
